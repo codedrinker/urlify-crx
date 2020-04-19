ecd99614fab29eb03f5d7a5e2c1573dc0ab6965b
codedrinker <gwangchunlei@gmail.com>
2020-04-20 07:25:28 +0800

    新增

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function onclick(info, tab) {
+function shortenLink(info, tab) {
     var uri = info.linkUrl || info.pageUrl;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://urlify.cn/chrome/extension", true);
@@ -15,14 +15,20 @@ function onclick(info, tab) {
                 copyFrom.select();
                 document.execCommand('copy');
                 body.removeChild(copyFrom);
-                sendMessageToContentScript();
+                sendMessageToContentScript(createMessage(1,1,"生成短链接成功并复制到剪切板"));
             } else {
-                sendMessageToContentScript(message);
+                sendMessageToContentScript(createMessage(1,0,message));
             }
         }
     }
     xhr.send(data);
 }
+
+function qrcode(info, tab) {
+    var uri = info.linkUrl || info.pageUrl;
+    sendMessageToContentScript(createMessage(2,1,uri));
+}
+
 
 function getCurrentTabId(callback) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
@@ -38,14 +44,34 @@ function sendMessageToContentScript(message, callback) {
     });
 }
 
+function createMessage(biz, code, msg) {
+    return JSON.stringify({
+            "biz": biz,
+            "code": code,
+            "msg": msg
+        });
+}
+
 chrome.contextMenus.create({
-    "title": "转换选中链接为短链接",
+    "title": "为选中链接生成短链",
     "contexts": ["link"],
-    "onclick": onclick
+    "onclick": shortenLink
 });
 
 chrome.contextMenus.create({
-    "title": "转换当前页面为短链接",
+    "title": "为选中链接生成二维码",
+    "contexts": ["link"],
+    "onclick": qrcode
+});
+
+chrome.contextMenus.create({
+    "title": "为此页面生成短链",
     "contexts": ["page"],
-    "onclick": onclick
+    "onclick": shortenLink
+});
+
+chrome.contextMenus.create({
+    "title": "为此页面生成二维码",
+    "contexts": ["page"],
+    "onclick": qrcode
 });
--- urlify.js
+++ urlify.js
@@ -1,6 +1,5 @@
 injectElement();
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
     showUrlifyMessage(request);
 });
 
@@ -11,26 +10,73 @@ function injectElement() {
 
     var urlifyAlert = document.createElement('div');
     urlifyAlert.className = "urlify-alert";
-
     urlifyContainer.appendChild(urlifyAlert);
     document.body.appendChild(urlifyContainer);
 }
 
+function createQrcodeElement(){
+    var urlifyQrcodeContainer = document.createElement('div');
+    urlifyQrcodeContainer.id = "urlify-qrcode-id";
+    urlifyQrcodeContainer.className = "urlify-qrcode";
+
+    var urlifyClose = document.createElement('div');
+    urlifyClose.className = "urlify-close";
+    urlifyClose.innerHTML = "X";
+    urlifyClose.onclick = function(){
+       removeQrcodeElement();
+    };
+    urlifyQrcodeContainer.appendChild(urlifyClose);
+    document.body.appendChild(urlifyQrcodeContainer);
+}
+
+function removeQrcodeElement() {
+    var element = document.getElementById("urlify-qrcode-id");
+    if(!!element) {
+        element.outerHTML = "";
+        delete element;   
+    }
+}
+
+
 function showUrlifyMessage(message) {
     var target = document.getElementById("urlify-alert-id");
-    if (!message) {
-        target.children[0].style.backgroundColor = "#81C784";
-        target.children[0].textContent = "生成短链接成功并复制到剪切板";
-    } else {
+    if (!!message) {
+        var msg = JSON.parse(message);
+        if(msg.biz == 1) {
+            if(msg.code == 1) {
+                target.children[0].style.backgroundColor = "#81C784";
+                target.children[0].textContent = msg.msg;
+                urlifyFadeIn(target, 10);
+                setTimeout(function() { urlifyFadeOut(target, 50) }, 1000);
+            } else {
+                target.children[0].style.backgroundColor = "#E57373";
+                target.children[0].textContent = msg.msg || "获取短链接失败请检查网络并重试";
+                urlifyFadeIn(target, 10);
+                setTimeout(function() { urlifyFadeOut(target, 50) }, 1000);
+            }
+        } else if(msg.biz == 2){
+            removeQrcodeElement();
+            createQrcodeElement();
+            new QRCode('urlify-qrcode-id', {
+                              text: msg.msg,
+                              width: 125,
+                              height: 125,
+                              colorDark : '#000000',
+                              colorLight : '#ffffff',
+                              correctLevel : QRCode.CorrectLevel.H
+                        });
+            urlifyFadeIn(document.getElementById("urlify-qrcode-id"), 10);
+        }
+    } else { 
         target.children[0].style.backgroundColor = "#E57373";
-        target.children[0].textContent = message || "获取短链接失败请检查网络并重试";
+        target.children[0].textContent = "处理失败，请重试";
+        urlifyFadeIn(target, 10);
+        setTimeout(function() { urlifyFadeOut(target, 50) }, 1000);
     }
-    urlifyFadeIn(target, 10);
-    setTimeout(function() { urlifyFadeOut(target, 50) }, 1000);
 }
 
 function setUrlifyOpacity(elem, opacity) {
-    if (elem.style.filter) { //IE
+    if (elem.style.filter) {
         elem.style.filter = 'alpha(opacity:' + opacity * 100 + ')';
     } else {
         elem.style.opacity = opacity;
